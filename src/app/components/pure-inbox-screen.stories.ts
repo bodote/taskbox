eb963ce56079739dc8b74cbce416fc2a20a873d4
Bodo Teichmann <bodo.teichmann@brandad-systems.de>
2021-11-01 16:57:04 +0100

    flip error true/false

--- src/app/components/pure-inbox-screen.stories.ts
+++ src/app/components/pure-inbox-screen.stories.ts
@@ -10,29 +10,29 @@ import { Store, NgxsModule } from '@ngxs/store';
 import { TasksState } from '../state/task.state';
 
 export default {
-    component: PureInboxScreenComponent,
-    decorators: [
-        moduleMetadata({
-            declarations: [PureInboxScreenComponent],
-            imports: [CommonModule, TaskModule, NgxsModule.forRoot([TasksState])],
-            providers: [Store],
-        }),
-        componentWrapperDecorator(
-            (story) => `<h2>Press D to show Actions and Controls Panel</h2><div style=" margin: 1em">${story}</div>`
-        ),
-    ],
-    title: 'PureInboxScreen',
+  component: PureInboxScreenComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [PureInboxScreenComponent],
+      imports: [CommonModule, TaskModule, NgxsModule.forRoot([TasksState])],
+      providers: [Store],
+    }),
+    componentWrapperDecorator(
+      (story) => `<h2>Press D to show Actions and Controls Panel</h2><div style=" margin: 1em">${story}</div>`
+    ),
+  ],
+  title: 'PureInboxScreen',
 } as Meta;
 
 const Template: Story<PureInboxScreenComponent> = (args) => ({
-    props: args,
+  props: args,
 });
 
 export const Default = Template.bind({});
 Default.args = {
-    error: true,
+  error: false,
 };
 export const Error = Template.bind({});
 Error.args = {
-    error: false,
+  error: true,
 };
