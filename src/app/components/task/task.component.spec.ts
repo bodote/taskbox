8fa578720f321826ef06ddf4ab458f6044c3c5b0
Bodo Teichmann <bodo.teichmann@brandad-systems.de>
2021-11-06 15:49:56 +0100

    added task test with jest-image-snapshot and puppeteer

--- src/app/components/task/task.component.spec.ts
+++ src/app/components/task/task.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskComponent } from './task.component';
+const puppeteer = require('puppeteer');
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
 describe('TaskComponent', () => {
   let component: TaskComponent;
   let fixture: ComponentFixture<TaskComponent>;
+  let browser;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,7 +15,7 @@ describe('TaskComponent', () => {
     }).compileComponents();
   });
 
-  beforeEach(() => {
+  beforeEach(async () => {
     fixture = TestBed.createComponent(TaskComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,5 +23,19 @@ describe('TaskComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should render ', () => {
+    expect(fixture).toMatchSnapshot(); // This is the snapshot validation
+    (expect(fixture) as any).toMatchImageSnapshot();
+  });
+  it('works with puppeteer', async () => {
+    expect.extend({ toMatchImageSnapshot });
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:6006/iframe.html?id=task--default&args=&viewMode=story');
+    const image = await page.screenshot();
+    (expect(image) as any).toMatchImageSnapshot();
+
+    await browser.close();
   });
 });
